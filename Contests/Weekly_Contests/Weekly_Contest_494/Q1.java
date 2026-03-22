a4d66d9b8ad6ec93be6c383ef9a198590aea1a07
SHAVVA VARSHITH REDDY <varshithreddy6147@gmail.com>
2026-03-22 20:43:31 +0530

    Add solution for Q1 - Construct Uniform Parity Array I

---
+++ Contests/Weekly_Contests/Weekly_Contest_494/Q1.java
@@ -0,0 +1,12 @@
+// Platform: LeetCode
+// Contest: Weekly Contest 494
+// Problem: Q1 - Construct Uniform Parity Array I
+// Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Difficulty: Easy
+// Topics: Math, Parity
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
